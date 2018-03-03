97aaad87ca76674d75738293f79c0f98e3c8c8b0
Benjamin Steenkamer <bsteen@udel.edu>
2018-03-03 07:47:19 +0000

    Removed stupid threading

--- hill2x2.py
+++ hill2x2.py
@@ -1,7 +1,7 @@
 import itertools
 import numpy as np
-import threading
-import time
+# import threading
+# import time
 import english_check
 
 # Character to number: a=0, ..., z=25
@@ -71,59 +71,64 @@ def test_key(matrix_key, vector_string, thread_id):
 
 # Given a list of key matrixes and an input string in the form of a list of 2x1
 # vectors, test all the keys on the input string
-# def test_all_keys(keys, vector_string):
-#     print("Testing all keys...")
-#     count = 1
+def test_all_keys(keys, vector_string):
+    print("Testing all", len(keys), "keys...")
+    output_file = open("output/hill2x2.txt", "w")
 
-#     for key in keys:
-#         test_key(key, vector_string, 0)
-#         if count % 1000 == 0:
-#             print("Number of keys tested:", count, "\n")
+    count = 1
+
+    for key in keys:
+        text_check, sqr_eng_sum, decoded = test_key(key, vector_string, 0)
+        if text_check:
+            output_file.writelines("Tested key:\n"+ str(key) + "\nSquared English sum:" + str(sqr_eng_sum) + "\nDecoded text: " + decoded + "\n\n")
+        if count % 1000 == 0:
+            print("Number of keys tested:", count, "/", len(keys))
+        count += 1
+
+    output_file.close()
+    print("Done testing all keys")
+    return
+
+# Worker function for a thread; Used by test_all_keys_threaded
+# def thread_worker(key_set, vector_string, start, end, thread_id):
+#     print("Starting thread:", thread_id, "Range:", start, end - 1)
+#     output_file = open("output/out_" + str(thread_id), "w")
+#     output_file.writelines("Thread: " + str(thread_id) + "\nRange: " + str(start) + " " + str(end - 1) + "\n\n")
+#     time.sleep(0.2)
+
+#     count = 0
+#     for key in key_set:
+#         text_check, sqr_eng_sum, decoded = test_key(key, vector_string, thread_id)
+#         if text_check:
+#             output_file.writelines("Tested key:\n"+ str(key) + "\nSquared English sum:" + str(sqr_eng_sum) + "\nDecoded text: " + decoded[:42] + "\n\n")
+
 #         count += 1
+#         if count % 1000 == 0:
+#             print("Thread " + str(thread_id) + " at " + str(count) + " of " + str(end - start))
+
+#     output_file.close()
+#     print("Thread", thread_id, "finished range:", start, end)
+#     return
+
+# Same as test_all_keys, except does it will multiple threads
+# def test_all_keys_threaded(keys, vector_string, num_threads):
+#     print("Testing all keys with", num_threads, "threads...")
+#     num_keys = len(keys)
+#     threads = []
+
+#     for thread_id in range(num_threads):
+#         start_idx = (num_keys // num_threads) * thread_id
+#         end_idx = (num_keys // num_threads) * thread_id + (num_keys // num_threads)
+#         new_thread = threading.Thread(target = thread_worker, args = (keys[start_idx:end_idx], vector_string, start_idx, end_idx, thread_id))
+#         threads.append(new_thread)
+#         new_thread.start()
+
+#     # Main thread blocks until all threads are finished
+#     for thread in threads:
+#         thread.join()
 
 #     print("Done testing all keys")
 #     return
-
-# Worker function for a thread; Used by test_all_keys_threaded
-def thread_worker(key_set, vector_string, start, end, thread_id):
-    print("Starting thread:", thread_id, "Range:", start, end - 1)
-    output_file = open("output/out_" + str(thread_id), "w")
-    output_file.writelines("Thread: " + str(thread_id) + "\nRange: " + str(start) + " " + str(end - 1) + "\n\n")
-    time.sleep(0.2)
-
-    count = 0
-    for key in key_set:
-        text_check, sqr_eng_sum, decoded = test_key(key, vector_string, thread_id)
-        if text_check:
-            output_file.writelines("Tested key:\n"+ str(key) + "\nSquared English sum:" + str(sqr_eng_sum) + "\nDecoded text: " + decoded[:42] + "\n\n")
-
-        count += 1
-        if count % 1000 == 0:
-            print("Thread " + str(thread_id) + " at " + str(count) + " of " + str(end - start))
-
-    output_file.close()
-    print("Thread", thread_id, "finished range:", start, end)
-    return
-
-# Same as test_all_keys, except does it will multiple threads
-def test_all_keys_threaded(keys, vector_string, num_threads):
-    print("Testing all keys with", num_threads, "threads...")
-    num_keys = len(keys)
-    threads = []
-
-    for thread_id in range(num_threads):
-        start_idx = (num_keys // num_threads) * thread_id
-        end_idx = (num_keys // num_threads) * thread_id + (num_keys // num_threads)
-        new_thread = threading.Thread(target = thread_worker, args = (keys[start_idx:end_idx], vector_string, start_idx, end_idx, thread_id))
-        threads.append(new_thread)
-        new_thread.start()
-
-    # Main thread blocks until all threads are finished
-    for thread in threads:
-        thread.join()
-
-    print("Done testing all keys")
-    return
 
 if __name__ == "__main__":
     string = ""
@@ -135,7 +140,7 @@ if __name__ == "__main__":
 
     keys = gen_all_key_matrixes()
     vector_string = string_to_vec(string)
-    test_all_keys_threaded(keys, vector_string, 12)     # Multithread brute force
+    test_all_keys(keys, vector_string)
 
     # vec_string = string_to_vec("plhzaoplzp")
     # matrix_key = np.matrix([[1,2],[3,4]])
